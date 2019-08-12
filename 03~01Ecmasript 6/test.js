9cd9dcda9b6bf7e51e471146ae7dcec8525f1670
liuyaohui <qq823406519@126.com>
2019-08-12 11:09:30 +0800

    19-8-12

--- 03~01Ecmasript 6/test.js
+++ 03~01Ecmasript 6/test.js
@@ -1,3 +1,24 @@
-let key1 = {},
-  key2 = {},
-  set = new WeakSet([key1, key2]);
+var text = 'hello1 hello2 hello3',
+  pattern = /hello\d\s?/,
+  result = pattern.exec(text),
+  globalPattern = /hello\d\s?/g,
+  globalResult = globalPattern.exec(text),
+  stickyPattern = /hello\d\s?/y,
+  stickyResult = stickyPattern.exec(text);
+
+console.log(result[0]); // "hello1 "
+console.log(globalResult[0]); // "hello1 "
+console.log(stickyResult[0]); // "hello1 "
+
+// ==== 指定了正则表达式对象的lastIndex后
+pattern.lastIndex = 1;
+globalPattern.lastIndex = 1;
+stickyPattern.lastIndex = 1;
+
+result = pattern.exec(text);
+globalResult = globalPattern.exec(text);
+stickyResult = stickyPattern.exec(text);
+
+console.log(result[0]); // "hello1 "
+console.log(globalResult[0]); // "hello2 "
+console.log(stickyResult[0]); // Error! stickyResult is null
